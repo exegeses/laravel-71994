5977c01c4993ce09621b5b6dc0025c208892f8f3
Marcos <exegeses@gmail.com>
2024-11-20 21:50:52 -0300

    Query Builder   - sintaxis básica && alta de un destino

--- agencia/resources/views/layouts/header.blade.php
+++ agencia/resources/views/layouts/header.blade.php
@@ -5,9 +5,23 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
+        <title>Agencia Laravel</title>
         <script src="https://cdn.tailwindcss.com"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
+        <link rel="shortcut icon" href="/imgs/m-iso.png" type="image/x-icon">
+        <link rel="stylesheet" href="/css/app.css">
     </head>
-    <body>
-        <x-navbar></x-navbar>
+    <body class="bg-gray-800 text-gray-200">
+        <header>
+            <nav class="bg-gray-800 shadow-lg">
+                <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                    <div class="relative flex h-16 items-center justify-between">
+
+                        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                            <x-logo></x-logo>
+                            <x-navbar></x-navbar>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
--- agencia/routes/web.php
+++ agencia/routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 //Route::view('peticion', nombreVista);
 //Route::view('/vista', 'vista');
@@ -41,4 +41,81 @@ Route::get('/proveedores', function ()
     $proveedores = DB::select('SELECT * FROM proveedores');
     //Retornamos la vista
     return view('proveedores', [ 'proveedores'=>$proveedores ]);
+});
+######################
+Route::view('/', 'plantilla');
+#####################
+### CRUD de regiones
+Route::get('/regiones', function ()
+{
+    //Obtenemos el listado de regiones
+    /* $regiones = DB::select('SELECT *
+                                FROM regiones
+                                ORDER BY idRegion DESC');*/
+    $regiones = DB::table('regiones')
+                        ->orderBy('idRegion','DESC')
+                        ->get();
+    //Retornamos la vista "regiones"
+    return view('regiones', [ 'regiones'=>$regiones ]);
+});
+Route::get('/destinos', function ()
+{
+    //Obtenemos listado de destinos
+    $destinos = DB::table('destinos as d')
+                        //->select('d.*', 'r.nombre as region')
+                        ->join('regiones as r', 'd.idRegion', '=', 'r.idRegion' )
+                        ->orderBy('idDestino', 'desc')
+                        ->get();
+    //Retornamos la vista destinos
+    return view('destinos', [ 'destinos'=>$destinos ]);
+});
+Route::get('/destino/create', function ()
+{
+    //Obtenemos listado de regiones
+    $regiones = DB::table('regiones')->get();
+    return view('destinoCreate', [ 'regiones'=>$regiones ]);
+});
+Route::post('/destino/store', function ()
+{
+    //Capturamos datos enviados por el form
+    // $aeropuero = request()->post('aeropuerto');
+    // $aeropuero = request()->input('aeropuerto');
+    // $aeropuerto = request()->aeropuerto;
+    $aeropuerto = request('aeropuerto');
+    $precio = request('precio');
+    $idRegion = request('idRegion');
+    try {
+        // raw sql
+        /* DB::insert('INSERT INTO destinos
+                        ( aeropuerto, precio, idRegion )
+                     VALUE
+                        ( :aeropuerto, :precio, :idRegion )',
+                        [ $aeropuerto, $precio, $idRegion ]
+                    );*/
+        DB::table('destinos')
+                    ->insert(
+                        [
+                           'aeropuerto'=>$aeropuerto,
+                           'precio'=>$precio,
+                           'idRegion'=>$idRegion,
+                            'activo'=>1
+                        ]
+                    );
+        return redirect('/destinos')
+                    ->with(
+                        [
+                            'css'=>'green',
+                            'mensaje'=>'Destino: '.$aeropuerto.' agregado correctamente',
+                        ]
+                    );
+
+    }catch( Throwable $th ){
+        return redirect('/destinos')
+            ->with(
+                [
+                    'css'=>'red',
+                    'mensaje'=>'No se pudo agregar el destino: '.$aeropuerto
+                ]
+            );
+    }
 });
